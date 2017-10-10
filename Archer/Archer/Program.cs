3bfb1b158e12587ebcddcee1fea8e1fddece78fa
istiophorus <rskotak@gmail.com>
2017-10-11 00:39:12 +0200

    Archer net

--- Archer/Archer/Program.cs
+++ Archer/Archer/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Archer
 {
@@ -11,15 +12,79 @@ namespace Archer
     {
         private static readonly Random _random = new Random(Environment.TickCount);
 
+        private static readonly int WindSpeedOffset = 10;
+
+        private static readonly int WindSpeedRange = 20;
+
+        private static readonly int MaxAngle = 90;
+
+        private static readonly int MinAngle = 0;
+
+        private static readonly int AngleRange = MaxAngle - MinAngle;
+
+        private static readonly int MaxWindSpeed = WindSpeedRange - WindSpeedOffset;
+
+        private static readonly int MinWindSpeed = -WindSpeedOffset;
+
+        private static readonly int MinDistance = 50;
+
+        private static readonly int MinSpeed = 0;
+
+        private static readonly int MaxSpeed = 200;
+
+        private static readonly int DistanceRange = 450;
+
+        private static readonly int MaxDistance = MinDistance + DistanceRange;
+
+        private const double Low = 0.0;
+
+        private const double High = 1.0;
+
+        private static double[][] WindSpeedWithOffsetBits = new []
+        {
+            new []{ Low, Low, Low, Low, Low },
+            new []{ Low, Low, Low, Low, High },
+            new []{ Low, Low, Low, High, Low },
+            new []{ Low, Low, Low, High, High },
+            new []{ Low, Low, High, Low, Low },
+            new []{ Low, Low, High, Low, High },
+            new []{ Low, Low, High, High, Low },
+            new []{ Low, Low, High, High, High },
+            new []{ Low, High, Low, Low, Low },
+            new []{ Low, High, Low, Low, High },
+            new []{ Low, High, Low, High, Low },
+            new []{ Low, High, Low, High, High },
+            new []{ Low, High, High, Low, Low },
+            new []{ Low, High, High, Low, High },
+            new []{ Low, High, High, High, Low },
+            new []{ Low, High, High, High, High },
+            new []{ High, Low, Low, Low, Low },
+            new []{ High, Low, Low, Low, High },
+            new []{ High, Low, Low, High, Low },
+            new []{ High, Low, Low, High, High },
+            new []{ High, Low, High, Low, Low },
+            new []{ High, Low, High, Low, High },
+            new []{ High, Low, High, High, Low },
+            new []{ High, Low, High, High, High },
+            new []{ High, High, Low, Low, Low },
+            new []{ High, High, Low, Low, High },
+            new []{ High, High, Low, High, Low },
+            new []{ High, High, Low, High, High },
+            new []{ High, High, High, Low, Low },
+            new []{ High, High, High, Low, High },
+            new []{ High, High, High, High, Low },
+            new []{ High, High, High, High, High }
+        };
+
         private static ProblemDefinition SingleTargetShootingWithWindTask()
         {
             TargetParameters targetParameters = new TargetParameters();
 
             targetParameters.TargetHeight = 2.0;
 
-            targetParameters.TargetDistance = _random.Next(450) + 50.0;
+            targetParameters.TargetDistance = _random.Next(DistanceRange) + MinDistance;
 
-            targetParameters.WindSpeed = _random.Next((int)20) - 10.0;
+            targetParameters.WindSpeed = _random.Next(WindSpeedRange) - WindSpeedOffset;
 
             ProblemDefinition result = ArcherProblemResolver.ResolveProblemAdvanced(targetParameters);
 
@@ -28,7 +93,71 @@ namespace Archer
 
         public static void Main()
         {
-            List<ProblemDefinition> learningData = PrepareData();
+            List<ProblemDefinition> records = PrepareData();
+
+            LearningData learningData = PrepareLearningData(records.ToArray());
+
+            TrainNetwork(learningData, @"..\Network\network.bin");
+        }
+
+        private static double EncodeValue(double value, double minValue, double maxValue)
+        {
+            return (value - minValue) / (maxValue - minValue) * (High - Low) + Low;
+        }
+
+        private static double[] EncodeProblemData(ProblemDefinition input)
+        {
+            double windSpeedEncoded = EncodeValue(input.Conditions.WindSpeed, MinWindSpeed, MaxWindSpeed);
+
+            double distanceEncoded = EncodeValue(input.Conditions.TargetDistance, MinDistance, MaxDistance);
+
+            return new double[] { windSpeedEncoded, distanceEncoded };
+        }
+
+        //private static double[] EncodeProblemData(ProblemDefinition input)
+        //{
+        //    int windSpeed = (int)input.Conditions.WindSpeed + WindSpeedOffset;
+
+        //    double[] windSpeedValues = WindSpeedWithOffsetBits[windSpeed];
+
+        //    List<double> result = new List<double>();
+
+        //    return result.ToArray();
+        //}
+
+        private static double[] EncodeProblemSolution(ProblemDefinition input)
+        {
+            double angleEncoded = EncodeValue(input.Solution.Angle, MinAngle, MaxAngle);
+
+            double speedEncoded = EncodeValue(input.Solution.InitialSpeed, MinSpeed, MaxSpeed);
+
+            return new double[] { angleEncoded, speedEncoded };
+        }
+
+        private static LearningData PrepareLearningData(ProblemDefinition[] inputData)
+        {
+            Double[][] input = new Double[inputData.Length][];
+
+            Double[][] output = new Double[inputData.Length][];
+
+            Parallel.For(0, inputData.Length, x =>
+            {
+                input[x] = EncodeProblemData(inputData[x]);
+                output[x] = EncodeProblemSolution(inputData[x]);
+            });
+
+            return new LearningData
+            {
+                Input = input,
+                Output = output
+            };
+        }
+
+        private sealed class LearningData
+        {
+            internal Double[][] Input { get; set; }
+
+            internal Double[][] Output { get; set; }
         }
 
         private static List<ProblemDefinition> PrepareData()
@@ -48,6 +177,8 @@ namespace Archer
 
             double average = problems.Select(x => x.Solution.Count).Average();
 
+            double maxSpeed = problems.Select(x => x.Solution.InitialSpeed).Max();
+
             return problems;
         }
 
@@ -57,9 +188,10 @@ namespace Archer
         {
             ActivationNetwork network = new ActivationNetwork(
                 UseBipolar ? (IActivationFunction)new BipolarSigmoidFunction(1) : (IActivationFunction)new SigmoidFunction(),
-                784,
-                784,
-                10);
+                2,
+                250,
+                //40,
+                2);
 
             network.Randomize();
 
@@ -81,7 +213,7 @@ namespace Archer
 
             Stopwatch sw = new Stopwatch();
 
-            Int32 counter = 100;
+            Int32 counter = 100000;
             // loop
             while (counter > 0)
             {
